2ac1f352be6d1b7b369b075f1a76b98c2ac4b36e
jgra007 <66275651+jgra007@users.noreply.github.com>
2022-02-03 12:07:55 -0500

    Update dashboard_main.js

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -7,12 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-
 let count = 0;
 
-function updateCounter() {count++ ;}
+function updateCounter() { count++; }
 
-$('button').clicks(_debounce(()=>{
-    updateCounter();
-    $('#count').text('${count} clicks on the button');
+$('button').click(_.debounce(() => {
+  updateCounter();
+  $('#count').text(`${count} clicks on the button`);
 }));
