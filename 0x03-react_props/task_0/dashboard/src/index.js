5a1721ffd0132bb3ecc5f6849736a70d3bae32f7
jgra007 <jr047x@gmail.com>
2022-02-15 18:46:19 -0500

    change name

--- 0x03-react_props/task_0/dashboard/src/index.js
+++ 0x03-react_props/task_0/dashboard/src/index.js
@@ -8,3 +8,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
